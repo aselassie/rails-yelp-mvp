33565eacbe49b79b24416de9be635c3872c57e4b
atsede selassie <atsede@gmail.com>
2020-01-23 18:51:57 +0000

    yelp done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  restaurants_detail = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'italian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'chinese'
+  },
+   {
+    name:         'Brindisa',
+    address:      '5 High Street,South Ken, London SW7 5JQ',
+    category:  'french'
+  },
+   {
+    name:         'Old Kent',
+    address:      '5 Smith Road, Brighton E1 6PQ',
+    category:  'belgian'
+  },
+   {
+    name:         'Nobu',
+    address:      '67 Mayfair, London W1 3WE',
+    category:  'japanese'
+  },
+]
+Restaurant.create!(restaurants_detail)
